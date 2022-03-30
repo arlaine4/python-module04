0f9cec1e966ea81254aa992ee9b012ea7a496025
Arthur LAINE <arlaine@student.42.fr>
2022-03-30 11:32:10 +0200

    finished ex03, howmanymedals

--- ex03/HowManyMedals.py
+++ ex03/HowManyMedals.py
@@ -1,18 +1,20 @@
 from copy import deepcopy
+import pandas as pd
 
 
 def howManyMedals(df, name):
+    if not isinstance(df, pd.DataFrame) or not isinstance(name, str):
+        return None
+    if 'Name' not in df.columns or 'Sex' not in df.columns or 'Medal' not in df.columns:
+        return None
     infos = df.filter(['Name', 'Medal', 'Year'])[(df.Name == name)]
     infos.dropna(inplace=True)
     infos.drop('Name', axis=1, inplace=True)
-    infos.Medal = infos.Medal.map(lambda name: name[0])
-    #infos = infos.set_index('Year')
-    sub_dict = {'G': 0, 'S': 0, 'B': 0}
-    dic = {}
-    # BORDEL BELOW
-    for y in infos.Year.unique():
-        dic[y] = sub_dict
-        dic[y] = infos[infos.Year == y].Medal.to_list()
-        print(infos[infos.Year == y].value_counts())
-    print(infos)
-    print(dic)
+    infos.Medal = infos.Medal.map(lambda n: n[0])
+    dic = {year:None for year in infos.Year.unique()}
+    for y in dic.keys():
+        medals_count = infos[infos.Year == y].Medal.to_list()
+        dic[y] = {'G': medals_count.count('G'),
+                  'S': medals_count.count('S'),
+                  'B': medals_count.count('B')}
+    return dic
