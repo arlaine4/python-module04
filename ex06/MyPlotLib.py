7ffc76cd8543763d110faee5545a4860263e2a6c
Arthur LAINE <arlaine@e2r11p8.clusters.42paris.fr>
2022-04-01 17:17:15 +0200

    finished ex06

--- ex06/MyPlotLib.py
+++ ex06/MyPlotLib.py
@@ -48,3 +48,35 @@ class MyPlotLib:
             sns.distplot(np.array(data[feature]), hist=False, label=feature)
             plt.legend()
         plt.show()
+
+    @staticmethod
+    def pair_plot(data, features):
+        if not isinstance(features, list) or not isinstance(data, pd.DataFrame):
+            return None
+        if len(features) <= 1:
+            sys.exit('Not enough features')
+        lst_features = [f for f in features if f in data.columns]
+        if len(lst_features) != len(features):
+            print('Invalid feature in feature list')
+            return None
+        df_features = data.filter([f for f in lst_features])
+        sns.pairplot(df_features, diag_kind='hist')
+        plt.show()
+
+    @staticmethod
+    def box_plot(data, features):
+        if not isinstance(features, list) or not isinstance(data, pd.DataFrame):
+            return None
+        if len(features) <= 1:
+            sys.exit('Not enough features')
+        lst_features = [f for f in features if f in data.columns]
+        if len(lst_features) != len(features):
+            print('Invalid feature in feature list')
+            return None
+        df_features = data.filter([f for f in lst_features])
+        df_features.dropna(inplace=True)
+        fig, ax = plt.subplots()
+        data = np.array(df_features)
+        ax.boxplot(data)
+        plt.xticks([i for i in range(1, len(features) + 1)], [feature for feature in features])
+        plt.show()
