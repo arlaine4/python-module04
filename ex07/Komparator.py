66dc8c5ea642fc82b3a94eaa8e3f32338c0831dd
arlaine4 <arthur.laine99@gmail.com>
2022-04-02 12:58:49 +0200

    finished compare_histograms method

--- ex07/Komparator.py
+++ ex07/Komparator.py
@@ -24,7 +24,6 @@ class Komparator:
         plt.legend()
         plt.show()
 
-
     def compare_histograms(self, categorical_var, numerical_var):
         if not isinstance(categorical_var, str) or not isinstance(numerical_var, str):
             return None
@@ -34,18 +33,25 @@ class Komparator:
         df = self.df.filter([numerical_var, categorical_var])
         df.dropna(subset=[numerical_var, categorical_var], inplace=True)
         fig = plt.figure(figsize=(12, 12))
-        sns.histplot(data=df, x=numerical_var, hue=categorical_var, multiple='stack') 
-        plt.show()
-        """print(single_values)
         single_values = list(df[categorical_var].unique())
-        x_values = [df[df[categorical_var] == value] for value in single_values]
-        print(df.Weight.shape, len(x_values[0]))
-        print(x_values)
-        plt.hist(x=x_values, y=df[numerical_var], labels=single_values)"""
-        """print(df.head())
-        for new_col in single_values:
-            df = df.insert(loc=1, column=new_col, value=np.array(df[df[categorical_var] == new_col]))
-        print(df.head())"""
-        #sns.histplot(data=df, x=numerical_var, hue=categorical_var, alpha=0.3)
-        #plt.title(f'{categorical_var} over {numerical_var}')
-        #plt.show()
+        for value in single_values:
+            plt.hist(np.array(df.filter([numerical_var])[df[categorical_var] == value]),
+                     alpha=0.5, label=value)
+        plt.xlabel(numerical_var)
+        plt.title(f'{categorical_var} over {numerical_var}')
+        plt.legend()
+        plt.show()
+
+    def compare_box_plots(self, categorical_var, numerical_var):
+        if not isinstance(categorical_var, str) or not isinstance(numerical_var, str):
+            return None
+        if categorical_var not in self.df.columns or numerical_var not in self.df.columns:
+            print(f'{categorical_var} or {numerical_var} not in df')
+            return None
+        df = self.df.filter([numerical_var, categorical_var])
+        df.dropna(subset=[categorical_var, numerical_var], inplace=True)
+        fig = plt.figure(figsize=(12, 12))
+        single_values = list(df[categorical_var].unique())
+        for value in single_values:
+            # TODO
+            pass
--- ex07/test.py
+++ ex07/test.py
@@ -4,5 +4,5 @@ from FileLoader import *
 fl = FileLoader()
 df = fl.load('athlete_events.csv')
 k = Komparator(df)
-# k.compare_histograms('Medal', 'Weight')
-k.density('Medal', 'Height')
+k.compare_histograms('Medal', 'Weight')
+# k.density('Medal', 'Height')
