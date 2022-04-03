7f8fce11a3d7ab3a2226479931343de700404f68
Arthur LAINE <arlaine@student.42.fr>
2022-04-03 13:26:33 +0200

    try except again, and a round for return float value

--- ex02/ProportionBySport.py
+++ ex02/ProportionBySport.py
@@ -1,7 +1,11 @@
 def proportionBySport(df, year, sport, gender):
-    subdf_all = df.filter(['Sport', 'Sex', 'Name', 'Year'])[(df.Year == year) & (df.Sex == gender)]
+    try:
+        subdf_all = df.filter(['Sport', 'Sex', 'Name', 'Year'])[(df.Year == year) & (df.Sex == gender)]
+    except KeyError:
+        print('Columns Sport, Sex, Name or Year not present in dataframe')
+        return None
     subdf_sport = subdf_all[(subdf_all.Sport == sport)]
     subdf_sport = subdf_sport.drop_duplicates(subset=['Name'])
     subdf_all = subdf_all.drop_duplicates(subset=['Name'])
-    return subdf_sport.shape[0] / subdf_all.shape[0]
+    return round(subdf_sport.shape[0] / subdf_all.shape[0], 4)
 
--- ex02/test.py
+++ ex02/test.py
@@ -1,8 +1,9 @@
 from FileLoader import FileLoader
 from ProportionBySport import proportionBySport
+import os
 
 fl = FileLoader()
-data = fl.load('../athlete_events.csv')
+data = fl.load(os.environ['CSV_PATH'])
 print('For 2004, Tennis and F:', end='\n')
 print(proportionBySport(data, 2004, 'Tennis', 'F'), end="\n\n")
 print('For 2008, Hockey and F:', end='\n')
