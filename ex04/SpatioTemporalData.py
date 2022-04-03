21ffa70b40315403c8da3429f66cda17632ff264
Arthur LAINE <arlaine@student.42.fr>
2022-04-03 13:44:17 +0200

    still new try excepts and some tests in test file

--- ex04/SpatioTemporalData.py
+++ ex04/SpatioTemporalData.py
@@ -5,13 +5,21 @@ class SpatioTemporalData:
     def when(self, location):
         if not isinstance(location, str):
             return None
-        tmp_df = self.df.filter(['Year', 'City'])[self.df.City == location]
+        try:
+            tmp_df = self.df.filter(['Year', 'City'])[self.df.City == location]
+        except KeyError:
+            print('Year or City not df columns')
+            return None
         tmp_df.drop_duplicates(subset=['Year'], inplace=True)
         return list(tmp_df.Year.unique())
 
     def where(self, date):
         if not isinstance(date, int):
             return None
-        tmp_df = self.df.filter(['Year', 'City'])[self.df.Year == date]
+        try:
+            tmp_df = self.df.filter(['Year', 'City'])[self.df.Year == date]
+        except KeyError:
+            print('Year or City not in df columns')
+            return None
         tmp_df.drop_duplicates(subset=['City'], inplace=True)
         return list(tmp_df.City.unique())
--- ex04/test.py
+++ ex04/test.py
@@ -1,12 +1,14 @@
 from FileLoader import FileLoader
 from SpatioTemporalData import SpatioTemporalData
+import os
 
 fl = FileLoader()
-data = fl.load('../athlete_events.csv')
+data = fl.load(os.environ['CSV_PATH'])
 sp = SpatioTemporalData(data)
-print(sp.where(1896))
-print(sp.where(2016))
-print(sp.where(0))
+print(sp.where(2000))
+print(sp.where(1980))
+print(sp.when('London'))
 print(sp.when('Athina'))
 print(sp.when('Paris'))
 print(sp.when('ah bon'))
+print(sp.where(0))
