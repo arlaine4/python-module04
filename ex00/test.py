ccb855fb5d20ffe5a4497060a05ce1cf5804885f
Arthur LAINE <arlaine@student.42.fr>
2022-04-03 13:19:18 +0200

    tests

--- ex00/test.py
+++ ex00/test.py
@@ -3,3 +3,7 @@ from FileLoader import FileLoader
 fl = FileLoader()
 data = fl.load('../athlete_events.csv')
 fl.display(data, 12)
+# fl.display(data, -12)
+# fl.display(data, 0)
+# fl.display(data, 'bonsoir')
+# fl.display(data, None)
